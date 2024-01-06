f1aaa834e7d7a9d351a0dc138dd3ab15b155967f
Bas aan de Wiel <bas.aan.de.wiel@gmail.com>
2024-01-06 21:13:52 +0100

    solved minor errors

--- functions/install_zigbee2mqtt.bash
+++ functions/install_zigbee2mqtt.bash
@@ -1,56 +1,50 @@
 #install zigbee2mqtt
-echo -p "answer yes to remove node-js BUT KEEP nodejs-lts-hydrogen" key
+read -p "answer yes to remove node-js BUT KEEP nodejs-lts-hydrogen" key
 sudo pacman -S --needed zigbee2mqtt
 
-echo -p "put in Zigbee USB stick (without USB extender; that can lead to crash is my experience)" keya
+node --version
+npm --version
+read -p "node must be v18 or higher; npm >=9.x" key
+sudo rm -rf /opt/zigbee2mqtt
+sudo mkdir /opt/zigbee2mqtt
+sudo chown -R ${USER}: /opt/zigbee2mqtt/
+git clone --depth 1 https://github.com/Koenkk/zigbee2mqtt.git /opt/zigbee2mqtt
+cd /opt/zigbee2mqtt/
+npm ci
+npm run build
+
+read -p "put in Zigbee USB stick (without USB extender; that can lead to crash is my experience)" keya
+read -p "find out serial port, like /dev/ttyAMA10 that USB stick uses, fr via dmesg, and fill in this device in /etc/zigbee2mqtt/configuration.yaml" key
+
+cat <<EOF > /opt/zigbee2mqtt/data/configuration.yaml
+homeassistant: false
+permit_join: true
+mqtt:
+  base_topic: zigbee2mqtt
+  server: mqtt://192.168.1.1
+serial:
+  port: /dev/ttyUSB0
+advanced:
+  homeassistant_legacy_entity_attributes: false
+  network_key: GENERATE
+  legacy_api: false
+  legacy_availability_payload: false
+frontedn:
+  port:4002
+device_options:
+      legacy: false
+EOF
+
+sudo hown -R zigbee2mqtt:zigbee2mqtt /opt/zigbee2mqtt
+
+
+#@@@ create service file
+
+
 sudo systemctl start zigbee2mqtt
 sudo systemctl enable zigbee2mqtt
 systemctl status zigbee2mqtt
 
-echo -p "check status" 
-
-
-    /etc/zigbee2mqtt/configuration.yaml
-        first add next to config
-            advanced:
-            network_key:GENERATE
-        after starting generates the network_key you can see below
-        homeassistant: false
-        permit_join: true
-        mqtt:
-          base_topic: zigbee2mqtt
-          server: mqtt://192.168.1.1
-        serial:
-          port: /dev/ttyUSB0
-        advanced:
-          network_key:
-            - 132
-            - 202
-            - 121
-            - 0
-            - 231
-            - 150
-            - 231
-            - 219
-            - 252
-            - 196
-            - 231
-            - 71
-            - 190
-            - 75
-            - 72
-            - 186
-          homeassistant_legacy_entity_attributes: false
-          legacy_api: false
-          legacy_availability_payload: false
-        frontend:
-          port: 4002
-        device_options:
-          legacy: false
-        devices:
-          '0xcc86ecfffeab7f88':
-            friendly_name: '0xcc86ecfffeab7f88'
-          '0xb4e3f9fffe780c32':
-            friendly_name: '0xb4e3f9fffe780c32'
+read -p "check status; als in con*.yaml a network key should be generated" key
 
 # front end available at port 4002 (port configured above)
